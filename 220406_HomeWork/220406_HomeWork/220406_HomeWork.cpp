43776aa9909e81dcd366749af679eee8a9b25607
csy-59 <csy010509@naver.com>
2022-04-06 22:11:30 +0900

    0406_Fix: 과제 완성

--- 220406_HomeWork/220406_HomeWork/220406_HomeWork.cpp
+++ 220406_HomeWork/220406_HomeWork/220406_HomeWork.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <string>
 using namespace std;
 
 int main()
@@ -7,12 +8,44 @@ int main()
 	* <<나에 대한 분석>>
 	*	1) 이름: 최서연
 	*	2) 나이: 22살
-	*	8) 키: 160.9
-	*	3) MBTI: INFP
-	*	4) 생일(년도): 2001
-	*	5) 생일(월): 5
-	*	6) 생일(일): 9
-	*	7) 좋아하는 색상: 검정색
-	*	9) 
+	*	3) 키: 160.9
+	*	4) MBTI: INFP
+	*	5) 혈액형: A
+	*	6) 생일(년도): 2001
+	*	7) 생일(월): 5
+	*	8) 생일(일): 9
+	*	9) 거주지: 하남시
+	*	10) 좋아하는 색상: 검정색
+	*	11) 좋아하는 동물: 고양이
+	*	12) 좋아하는 계절: 가을
+	*	13) 가장 좋아하는 영화: 너는 여기에 없었다.
+	*	14) 가장 좋아하는 미드: 피키 블라인더스 
+	*	15) 가장 좋아하는 애니메이션: 보잭 홀스맨
+	*	16) 가장 좋아하는 꽃: 장미
+	*	17) 싫어하는 것: 콩 
+	*	18) 장점: 끈기
+	*	19) 단점: 완벽주의
+	*	20) 목표: AR/VR 개발자
 	*/
+
+	string name = "최서연";
+	int age = 22;
+	float height = 160.9f;
+	string mbti = "INFP";
+	char bloodType = 'A';
+	int birthYear = 2001;
+	int birthMonth = 5;
+	int birthDay = 9;
+	string residence = "Hanam";
+	string favoriteColor = "Black";
+	string favoriteAnimal = "Cat";
+	string favoriteSeason = "Fall";
+	string favoriteMovie = "You were Never Really Here";
+	string favoriteDrama = "Peaky Blinders";
+	string favoriteAnim = "Bojack Horseman";
+	string favoriteFlower = "Rose";
+	string dislike = "Bean";
+	string strength = "Persistence";
+	string weakness = "Perfectionism";
+	string goal = "AR/VR Developer";
 }
