10bd8efc5f2e8f72276c962f335a1dcec4dd2125
csy-59 <csy010509@naver.com>
2022-04-18 17:39:25 +0900

    0418_FIX: 약간 수정

--- Project1_Splendor/Project1_Splendor/Player.h
+++ Project1_Splendor/Project1_Splendor/Player.h
@@ -1,30 +1,13 @@
 #pragma once
-#include <iostream>
-#include <string>
+#ifndef PLAYER
+#define PLAYER
 using namespace std;
 
-class Player
-{
-	//플레이어 클래스
-
-	//멤버 변수
-	string name; //플레이어 이름
-	int turn; //플레이어 순서
-
-	Player(string name, int turn) {
-		this->name = name;
-		this->turn = turn;
-	}
-	Player() {
-		name = "";
-		turn = -1;
-	}
-
-	void setName(string name) {
-		this->name = name;
-	}
-	void setTurn(int turn) {
-		this->turn = turn;
-	}
+class Player {
+private:
+	string name;
+	int turn;
 };
+#endif // !PLAYER
+
 
