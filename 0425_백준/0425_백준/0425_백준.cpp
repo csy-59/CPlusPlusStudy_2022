2c23df70dd915534add538f42e9f3be7260da38c
csy-59 <csy010509@naver.com>
2022-04-26 09:14:16 +0900

    0426_Add: 백준용 연습 프로젝트 생성

---
+++ 0425_백준/0425_백준/0425_백준.cpp
@@ -0,0 +1,13 @@
+﻿#pragma warning(disable : 4996)
+#include <stdio.h>
+
+int main() {
+
+    int a, b;
+    while (1) {
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a + b);
+    }
+
+    return 0;
+}
